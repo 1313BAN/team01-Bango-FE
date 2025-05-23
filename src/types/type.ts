a2c01121d18c5cbe8c2dff59887868e5da3c5abc
J2yun <alex68566856@gmail.com>
2025-05-23 16:19:28 +0900

    [CHORE] dongcode 파일명 수정

---
+++ src/types/type.ts
@@ -0,0 +1,12 @@
+// 데이터 타입
+export type DongCode = {
+  dongCode: string;
+  sidoName: string;
+  gugunName: string;
+  dongName: string;
+};
+
+export type AuthToken = {
+  accessToken: string;
+  refreshToken: string;
+};

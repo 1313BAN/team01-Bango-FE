505b2bfa12fe7af282a82bdd7b9ccd9ff18e2a46
J2yun <alex68566856@gmail.com>
2025-05-23 17:09:15 +0900

    [CHORE] pinia-plugin-persistedstate 추가, js-cookie 제거

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+const app = createApp(App);
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
